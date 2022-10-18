2b9f7bc9c6afce1be810d9dfbd0e737a9fb25c7d
isacchr <isac.christiansson@gmail.com>
2022-10-18 12:18:54 +0200

    navbar added but icons not working yet

--- public/app.js
+++ public/app.js
@@ -5,7 +5,7 @@ login = () => {
     XHR.onload = () => {
 
         const divContent = document.getElementById('app');
-        
+
         divContent.innerHTML = XHR.responseText;
 
     }
@@ -13,9 +13,23 @@ login = () => {
     XHR.open('GET', '/home');
     XHR.send();
 
+
 };
-
-
+ 
 createAccount = () => {
 
 };
+
+/*buildNavbar = () => {
+
+    const divNavbar = document.getElementById('navbar');
+
+    const navbar = document.createElement('nav');
+    navbar.className = 'navbar navbar-light bg-light';
+    divNavbar.appendChild(navbar);
+
+    const homeIcon = document.createElement('a');
+    homeIcon.className = 'navbar-brand';
+    navbar.appendChild(homeIcon);
+
+}*/
