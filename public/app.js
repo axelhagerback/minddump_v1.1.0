cd1a0d07c6c5d5321d3127953f4aea064cc3d6c7
axelhagerback <axel.hagerback@gmail.com>
2022-10-24 14:14:57 +0200

    session and cookies added to site for login purpose

--- public/app.js
+++ public/app.js
@@ -1,194 +1,178 @@
 login = () => {
-    var userInfo;
+  var userInfo;
 
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
 
-    var infoOrder = {Email: email, Password: password};
-    userInfo = infoOrder;
+  var infoOrder = {
+    Email: email,
+    Password: password,
+    username: email,
+    password: password,
+  };
+  userInfo = infoOrder;
 
-    const XHR = new XMLHttpRequest(); 
+  const XHR = new XMLHttpRequest();
 
-    XHR.onload = () => {
+  XHR.onload = () => {
+    const divContent = document.getElementById("app");
+    const response = XHR.responseText;
 
-        const divContent = document.getElementById('app');
-        const response = XHR.responseText;
-        
-        if (response == 'Wrong') {
-            console.log(response);
-        } else {
-            divContent.innerHTML = response;
-        };
-    };
+    if (response == "Wrong") {
+      console.log(response);
+    } else {
+      divContent.innerHTML = response;
+    }
+  };
 
-    XHR.open('POST', '/home');
-    XHR.setRequestHeader('Content-type', 'application/json')
-    XHR.send(JSON.stringify(userInfo));
+  XHR.open("POST", "/home");
+  XHR.setRequestHeader("Content-type", "application/json");
+  XHR.send(JSON.stringify(userInfo));
 
-    document.getElementById('email').value = '';
-    document.getElementById('password').value = '';
-
+  document.getElementById("email").value = "";
+  document.getElementById("password").value = "";
 };
 
 dirHome = () => {
-    const XHR = new XMLHttpRequest();
+  const XHR = new XMLHttpRequest();
 
-    XHR.onload = () => {
-        const divContent = document.getElementById('app');
-        divContent.innerHTML = XHR.responseText;
-    };
+  XHR.onload = () => {
+    const divContent = document.getElementById("app");
+    divContent.innerHTML = XHR.responseText;
+  };
 
-    XHR.open('GET', '/home');
-    XHR.send();
+  XHR.open("GET", "/home");
+  XHR.send();
 };
- 
+
 formCreateAccount = () => {
-    createAccountBox.hidden = false;
+  createAccountBox.hidden = false;
 };
 
 createAccount = () => {
-    var userInfo;
+  var userInfo;
 
-    var email = document.getElementById('userEmail').value;
-    var password = document.getElementById('userPassword').value;
+  var email = document.getElementById("userEmail").value;
+  var password = document.getElementById("userPassword").value;
 
-    var infoOrder = {Email: email, Password: password};
-    userInfo = infoOrder;
+  var infoOrder = { Email: email, Password: password };
+  userInfo = infoOrder;
 
+  const XHR = new XMLHttpRequest();
 
-    const XHR = new XMLHttpRequest();
+  XHR.onload = () => {
+    const response = XHR.responseText;
+    console.log(response);
+    if (response == "Already exists") {
+      const createAccountDiv = document.getElementById("createAccountBox");
+      if (createAccountDiv != null) {
+        createAccountDiv.classList.add("shakeCSS");
+        createAccountDiv.classList.toggle("shakeCSS");
+        setTimeout(() => createAccountDiv.classList.toggle("shakeCSS"), 100);
+        setTimeout(() => createAccountDiv.classList.toggle("shakeCSS"), 1500);
+      }
+    }
+  };
 
-    XHR.onload = () => {
-        const response = XHR.responseText;
-        console.log(response);
-        if (response == 'Already exists') {
-            const createAccountDiv = document.getElementById('createAccountBox');
-            if (createAccountDiv != null) {
-                createAccountDiv.classList.add('shakeCSS');
-                createAccountDiv.classList.toggle('shakeCSS');
-                setTimeout(() => (createAccountDiv.classList.toggle('shakeCSS')), 100)
-                setTimeout(() => (createAccountDiv.classList.toggle('shakeCSS')), 1500)            }
-            };
-    };
+  XHR.open("POST", "/addUser");
+  XHR.setRequestHeader("Content-type", "application/json");
+  XHR.send(JSON.stringify(userInfo));
 
-    XHR.open('POST', '/addUser');
-    XHR.setRequestHeader('Content-type', 'application/json')
-    XHR.send(JSON.stringify(userInfo));
-
-    document.getElementById('userEmail').value = '';
-    document.getElementById('userPassword').value = '';
+  document.getElementById("userEmail").value = "";
+  document.getElementById("userPassword").value = "";
 };
 
 create = () => {
+  var title = document.getElementById("title").value;
+  var post = document.getElementById("post").value;
 
-    var title = document.getElementById('title').value;
-    var post = document.getElementById('post').value;
+  var noteInfo = { Title: title, Post: post };
 
-    var noteInfo = {Title: title, Post: post};
+  const XHR = new XMLHttpRequest();
 
-    const XHR = new XMLHttpRequest();
+  XHR.onload = () => {
+    const response = XHR.responseText;
+    console.log(response);
+  };
 
-    XHR.onload = () => {
-        const response = XHR.responseText;
-        console.log(response);
-    }
-
-    XHR.open('POST', '/createNote');
-    XHR.setRequestHeader('Content-type', 'application/json')
-    XHR.send(JSON.stringify(noteInfo));
+  XHR.open("POST", "/createNote");
+  XHR.setRequestHeader("Content-type", "application/json");
+  XHR.send(JSON.stringify(noteInfo));
 };
 
-
-
-
 myNotes = () => {
+  const XHR = new XMLHttpRequest();
 
-    const XHR = new XMLHttpRequest();
+  XHR.onload = () => {
+    const divContent = document.getElementById("app");
+    divContent.innerHTML = XHR.responseText;
+    buildNotes();
+  };
 
-    XHR.onload = () => {
-
-        const divContent = document.getElementById('app');
-        divContent.innerHTML = XHR.responseText;
-        buildNotes();
-    }
-
-    XHR.open('GET', '/myNotes');
-    XHR.send();
-
+  XHR.open("GET", "/myNotes");
+  XHR.send();
 };
 
 buildNotes = () => {
-    const XHR = new XMLHttpRequest();
+  const XHR = new XMLHttpRequest();
 
+  //skapa variabel med responsetext
 
-    //skapa variabel med responsetext
-    
-    const userNotes = document.getElementById('userNotes');
-    //const indNote = document.getElementById('note');
-    
+  const userNotes = document.getElementById("userNotes");
+  //const indNote = document.getElementById('note');
 
-    XHR.onload = () => {
-       
-        
-        var Notes = JSON.parse(XHR.responseText);
-        userNotes.innerHTML = '';
+  XHR.onload = () => {
+    var Notes = JSON.parse(XHR.responseText);
+    userNotes.innerHTML = "";
 
-        Notes.forEach(note => {
-            const noteDiv = document.createElement('div');
-            const noteHeader = document.createElement('div');
-            const noteMain = document.createElement('div');
-            const noteFooter = document.createElement('div');
-            const editbtn = document.createElement('button');
-            const delbtn = document.createElement('button');
-            const btnDiv  = document.createElement('div');
+    Notes.forEach((note) => {
+      const noteDiv = document.createElement("div");
+      const noteHeader = document.createElement("div");
+      const noteMain = document.createElement("div");
+      const noteFooter = document.createElement("div");
+      const editbtn = document.createElement("button");
+      const delbtn = document.createElement("button");
+      const btnDiv = document.createElement("div");
 
+      noteDiv.setAttribute("class", "noteDiv");
+      noteHeader.setAttribute("class", "noteHeader");
+      noteMain.setAttribute("class", "userNote");
+      noteFooter.setAttribute("class", "noteFooter");
+      editbtn.setAttribute("class", "editbtn");
+      delbtn.setAttribute("class", "delbtn");
+      btnDiv.setAttribute("class", "btnDiv");
 
-            noteDiv.setAttribute('class', 'noteDiv');
-            noteHeader.setAttribute('class', 'noteHeader');
-            noteMain.setAttribute('class', 'userNote');
-            noteFooter.setAttribute('class', 'noteFooter');
-            editbtn.setAttribute('class', 'editbtn');
-            delbtn.setAttribute('class', 'delbtn');
-            btnDiv.setAttribute('class', 'btnDiv');
+      noteHeader.innerHTML = `${note.Title}`;
+      noteMain.innerHTML = `${note.Note}`;
+      noteFooter.innerHTML = `${note.Date}`;
+      editbtn.innerHTML = "Edit";
+      delbtn.innerHTML = "Delete";
 
-            noteHeader.innerHTML = `${note.Title}`;
-            noteMain.innerHTML = `${note.Note}`;
-            noteFooter.innerHTML = `${note.Date}`;
-            editbtn.innerHTML = 'Edit';
-            delbtn.innerHTML = 'Delete';
+      noteDiv.append(noteHeader);
+      noteDiv.append(noteMain);
+      noteDiv.append(noteFooter);
+      btnDiv.append(editbtn);
+      userNotes.append(noteDiv);
+      btnDiv.append(delbtn);
+      noteFooter.append(btnDiv);
+    });
+  };
 
-
-            noteDiv.append(noteHeader);
-            noteDiv.append(noteMain);
-            noteDiv.append(noteFooter);
-            btnDiv.append(editbtn);
-            userNotes.append(noteDiv);
-            btnDiv.append(delbtn);
-            noteFooter.append(btnDiv);
-        });
-        
-    };
-
-    
-    XHR.open('GET', '/notes');
-    XHR.send();
+  XHR.open("GET", "/notes");
+  XHR.send();
 };
-
 
 logout = () => {
-    const XHR = new XMLHttpRequest();
+  const XHR = new XMLHttpRequest();
 
-    XHR.onload = () => {
+  XHR.onload = () => {
+    const divContent = document.getElementById("app");
+    divContent.innerHTML = XHR.responseText;
+  };
 
-        const divContent = document.getElementById('app');
-        divContent.innerHTML = XHR.responseText;
-
-    }
-
-    XHR.open('GET', '/');
-    XHR.send();
+  XHR.open("GET", "/");
+  XHR.send();
 };
-
 
 /*buildNavbar = () => {
 
--- server.js
+++ server.js
@@ -1,142 +1,210 @@
-const express = require('express');
+const express = require("express");
 const cookieParser = require("cookie-parser");
-const session = require ('express-session');
+const session = require("express-session");
 const server = express();
-const fs = require('fs');
-require('dotenv').config()
-const Airtable = require('airtable');
-const bcrypt = require('bcrypt');
+const fs = require("fs");
+require("dotenv").config();
+const Airtable = require("airtable");
+const bcrypt = require("bcrypt");
+const localStrategy = require("passport-local");
+const passport = require("passport");
 
 const timer = 1000 * 60 * 30;
 
 var notesArray = [];
 
-var baseNotes = new Airtable({apiKey: process.env.API_KEY}).base('appe1p4d3ipTDbCef');
-var baseUsers = new Airtable({apiKey: process.env.API_KEY}).base('app3AWTTb59zksQDR');
+var baseNotes = new Airtable({ apiKey: process.env.API_KEY }).base(
+  "appe1p4d3ipTDbCef"
+);
+var baseUsers = new Airtable({ apiKey: process.env.API_KEY }).base(
+  "app3AWTTb59zksQDR"
+);
 
-server.use(express.static('public'));
+var strategy = new localStrategy((username, password, cb) => {
+  baseUsers("users")
+    .select({
+      filterByFormula: `Email="${username}"`,
+    })
+    .eachPage((records, processNextPage) => {
+      if (bcrypt.compareSync(password, records[0].get("Password"))) {
+        console.log("Correct credentials");
+      } else {
+        return cb(null, false);
+      }
+      return cb(null, { id: records[0].id });
+    });
+});
+
+passport.serializeUser((user, cb) => {
+  return cb(null, {
+    id: user.id,
+  });
+});
+
+passport.deserializeUser((user, cb) => {
+  return cb(null, {
+    id: user.id,
+  });
+});
+
+passport.use(strategy);
+server.use(express.static("public"));
 server.use(express.json());
-server.use(express.urlencoded({extended: true}));
+server.use(express.urlencoded({ extended: true }));
 server.use(cookieParser());
-server.use(session({
+server.use(
+  session({
     secret: process.env.SECRET,
-    saveUninitialized:false,
+    saveUninitialized: false,
     cookie: { maxAge: timer },
-    resave: false
-}));
+    resave: false,
+  })
+);
+server.use(passport.initialize());
+server.use(passport.session());
 
-server.get('/', (req, res) => {
-    fs.readFile('index.html','utf-8', (err, data) => {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        res.end();
+server.get("/", (req, res) => {
+  if (req.user) {
+    return res.redirect("/home");
+  } else {
+    fs.readFile("index.html", "utf-8", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
     });
-}); 
-
-server.post('/home', (req, res) => {
-
-    baseUsers('users').select({
-        filterByFormula: `Email="${userLoginInfo.Email}"`
-    }).eachPage((records, processNextPage) => {
-            bcrypt.compare(userLoginInfo.Password, records[0].get('Password'), (err, response) => {
-                if (response) {
-                    fs.readFile('home.html', 'utf-8', (err, data) => {
-                        res.writeHead(200, {'Content-Type': 'text/html'});
-                        res.write(data);
-                        res.end();
-                        return;
-                    });
-                } else {
-                res.send('Wrong');
-                return;
-            };
-        });
-    }).catch((err) => console.log(err));
-
+  }
 });
 
-
-server.get('/home', (req, res) => {
-    fs.readFile('home.html', 'utf-8', (err, data) => {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        res.end();
-    });
-});
-
-server.get('/myNotes', (req, res) => {
-
-    fs.readFile('myNotes.html', 'utf-8', (err, data) => {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        res.end();
-    });
-
-});
-
-server.post('/addUser', (req, res) => {
-
+server.post(
+  "/home",
+  passport.authenticate("local", {
+    failureRedirect: "/",
+    failureMessage: true,
+  }),
+  (req, res) => {
     var userLoginInfo = req.body;
-   
-    baseUsers('users').select({
-        filterByFormula: `Email="${userLoginInfo.Email}"`
-    }).eachPage((records, processNextPage) => {
-        if (records.length > 0) {
-            res.send('Already exists');
-            return;
-        }
-        bcrypt.hash(userLoginInfo.Password, 10, (err, hash) => {
-            if (err) {
-                console.error(err);
+
+    baseUsers("users")
+      .select({
+        filterByFormula: `Email="${userLoginInfo.Email}"`,
+      })
+      .eachPage((records, processNextPage) => {
+        bcrypt.compare(
+          userLoginInfo.Password,
+          records[0].get("Password"),
+          (err, response) => {
+            if (response) {
+              fs.readFile("home.html", "utf-8", (err, data) => {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.write(data);
+                res.end();
                 return;
-            };
-    
-            baseUsers('users').create([
-    
-                {
-                    "fields" : {
-                        "Email": userLoginInfo.Email,
-                        "Password": hash
-                    }
-                }
-        
-            ], function(err, records) {
-                if (err) {
-                    console.error(err);
-                    return;
-                };
-            });
-            
-        });
-    }).catch((err) => console.log(err));
-          
+              });
+            } else {
+              res.send("Wrong");
+              return;
+            }
+          }
+        );
+      })
+      .catch((err) => console.log(err));
+  }
+);
+
+server.get("/home", (req, res) => {
+  if (req.user) {
+    fs.readFile("home.html", "utf-8", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  } else {
+    return res.redirect("/");
+  }
 });
 
-server.post('/createNote', (req, res) => {
-    var noteInfo = req.body;
-    
-    res.send(noteInfo);
+server.get("/myNotes", (req, res) => {
+  if (req.user) {
+    fs.readFile("myNotes.html", "utf-8", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  } else {
+    return res.redirect("/");
+  }
 });
 
+server.post("/addUser", (req, res) => {
+  var userLoginInfo = req.body;
 
-server.get('/notes', (req, res) => {
-    notesArray = [];
+  baseUsers("users")
+    .select({
+      filterByFormula: `Email="${userLoginInfo.Email}"`,
+    })
+    .eachPage((records, processNextPage) => {
+      if (records.length > 0) {
+        res.send("Already exists");
+        return;
+      }
+      bcrypt.hash(userLoginInfo.Password, 10, (err, hash) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
 
-    baseNotes('Notes').select({
-        view: 'Grid view'
-    }).firstPage((err, records) => {
-        if (err) { console.error(err); return; }
-            records.forEach((record) => {
+        baseUsers("users").create(
+          [
+            {
+              fields: {
+                Email: userLoginInfo.Email,
+                Password: hash,
+              },
+            },
+          ],
+          function (err, records) {
+            if (err) {
+              console.error(err);
+              return;
+            }
+          }
+        );
+      });
+    })
+    .catch((err) => console.log(err));
+});
 
-            var note = {UserId: record.get('UserId'), Title: record.get('Title'), Note: record.get('Note'), Date: record.get('Date')};
-            notesArray.push(note);
-        });
+server.post("/createNote", (req, res) => {
+  var noteInfo = req.body;
 
-        res.send(notesArray);
+  res.send(noteInfo);
+});
+
+server.get("/notes", (req, res) => {
+  notesArray = [];
+
+  baseNotes("Notes")
+    .select({
+      view: "Grid view",
+    })
+    .firstPage((err, records) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      records.forEach((record) => {
+        var note = {
+          UserId: record.get("UserId"),
+          Title: record.get("Title"),
+          Note: record.get("Note"),
+          Date: record.get("Date"),
+        };
+        notesArray.push(note);
+      });
+
+      res.send(notesArray);
     });
 });
-
-
 
 server.listen(8080);
-console.log('Server is running');
+console.log("Server is running");
